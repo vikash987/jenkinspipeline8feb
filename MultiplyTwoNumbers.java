4f9ad13fb6ab3cb6ece562b03b2bc1310018581c
vikash987 <40848251+vikash987@users.noreply.github.com>
2026-02-08 11:20:41 +0530

    Update MultiplyTwoNumbers.java

--- MultiplyTwoNumbers.java
+++ MultiplyTwoNumbers.java
@@ -2,8 +2,8 @@ class MultiplyTwoNumbers {
 
   public static void main(String[] args) {
     
-    int first = 10;
-    int second = 20;
+    int first = 10.5;
+    int second = 21.3;
 
     // add two numbers
     int sum = first + second;
